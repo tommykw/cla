1d37e60c7519bcd0639651ca7208ef6e74957fcd
kenji tomita <destinys.lotus.1108mn@gmail.com>
2017-01-12 07:30:48 +0900

    Fix launcher

--- src/main/kotlin/Launcher.kt
+++ src/main/kotlin/Launcher.kt
@@ -1,11 +1,10 @@
 import javax.swing.SwingUtilities
 
-class Launcher {
-    companion object {
-        fun main(args: Array<String>) {
-            SwingUtilities.invokeLater {
-                Keditor().isVisible = true
-            }
+object Launcher {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        SwingUtilities.invokeLater {
+            Keditor().isVisible = true
         }
     }
 }
